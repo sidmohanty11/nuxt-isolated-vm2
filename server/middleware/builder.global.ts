9d0d6be325ed5276694f7c1b7aa44adde9e17b70
sidmohanty11 <sidmohanty11@gmail.com>
2024-10-07 12:14:41 +0530

    use manual approach

--- server/middleware/builder.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async (event) => {
-  if (process.server) {
-    const { initializeNodeRuntime } = await import(
-      "@builder.io/sdk-vue/node/init"
-    );
-    initializeNodeRuntime();
-  }
-});
