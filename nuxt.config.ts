e4f533c39ccc0e7657c7640914deb64397944679
sidmohanty11 <sidmohanty11@gmail.com>
2024-10-08 16:26:15 +0530

    vercel preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   nitro: {
-    preset: "vercel_edge",
+    preset: "vercel",
   },
   modules: [
     [
