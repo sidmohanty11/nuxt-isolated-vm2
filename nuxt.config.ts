1605fb6962903e6a93a2fdbbac61e5a7c54b6bed
sidmohanty11 <sidmohanty11@gmail.com>
2024-10-08 16:09:17 +0530

    use vercel preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  nitro: {
+    preset: "vercel",
+  },
   modules: [
     [
       "@builder.io/sdk-vue/nuxt",
       {
+        includeCompiledCss: true,
         initializeNodeRuntime: true,
       },
     ],
